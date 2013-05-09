cab7903c96df19361a1cbdb30a6a312501a731b0
Sam Wong <swong@vitalinktech.com>
2013-05-10 00:12:48 +0800

    SAM3XA: ADC implementation

---
+++ os/hal/platforms/SAM3XA/adc_lld.c
@@ -0,0 +1,202 @@
+/*
+    ChibiOS/RT - Copyright (C) 2013 Sam Wong
+
+    This file is part of Arduino Due patch for ChibiOS/RT.
+
+    ChibiOS/RT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS/RT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+                                      ---
+
+    A special exception to the GPL can be applied should you wish to distribute
+    a combined work that includes ChibiOS/RT, without being obliged to provide
+    the source code for any proprietary components. See the file exception.txt
+    for full details of how and when the exception can be applied.
+ */
+
+/**
+ * @file    SAM3XA/adc_lld.c
+ * @brief   SAM3XA ADC Driver subsystem low level driver source.
+ *
+ * @addtogroup ADC
+ * @{
+ */
+
+#include "ch.h"
+#include "hal.h"
+
+#if HAL_USE_ADC || defined(__DOXYGEN__)
+
+/*===========================================================================*/
+/* Driver local definitions.                                                 */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver exported variables.                                                */
+/*===========================================================================*/
+
+/**
+ * @brief   ADC1 driver identifier.
+ */
+#if SAM3XA_ADC_USE_ADC1 || defined(__DOXYGEN__)
+ADCDriver ADCD1;
+#endif
+
+/*===========================================================================*/
+/* Driver local variables and types.                                         */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver local functions.                                                   */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver interrupt handlers.                                                */
+/*===========================================================================*/
+
+CH_IRQ_HANDLER(SAM3XA_ADC_HANDLER) {
+  uint32_t isr;
+  CH_IRQ_PROLOGUE();
+
+  isr = ADC->ADC_ISR;
+  if (ADCD1.grpp) {
+    /* ADC overflow condition, this could happen only if the DMA is unable to read data fast enough.*/
+    if ((isr & ADC_ISR_GOVRE)) {
+      _adc_isr_error_code(&ADCD1, ADC_ERR_OVERFLOW);
+      adc_lld_stop_conversion(&ADCD1);
+    } else if ((isr & ADC_ISR_RXBUFF)) {
+      _adc_isr_full_code(&ADCD1);
+    }
+  }
+
+  CH_IRQ_EPILOGUE();
+}
+
+/*===========================================================================*/
+/* Driver exported functions.                                                */
+/*===========================================================================*/
+
+/**
+ * @brief   Low level ADC driver initialization.
+ *
+ * @notapi
+ */
+void adc_lld_init(void) {
+#if PLATFORM_ADC_USE_ADC1
+  /* Driver initialization.*/
+  adcObjectInit(&ADCD1);
+#endif /* PLATFORM_ADC_USE_ADC1 */
+}
+
+/**
+ * @brief   Configures and activates the ADC peripheral.
+ *
+ * @param[in] adcp      pointer to the @p ADCDriver object
+ *
+ * @notapi
+ */
+void adc_lld_start(ADCDriver *adcp) {
+  uint32_t clock;
+
+  // Enable clock and interrupt vector
+  pmc_enable_peripheral_clock(ID_ADC);
+  nvicEnableVector(ADC_IRQn, CORTEX_PRIORITY_MASK(SAM3XA_ADC_IRQ_PRIORITY));
+
+  if (adcp->state == ADC_STOP) {
+    /* Enables the peripheral.*/
+    // Clear the ISR
+    uint16_t dummy;
+    uint8_t i;
+    dummy = ADC->ADC_LCDR;
+    for (i = 0; i < 16; i++) {
+      dummy = ADC->ADC_CDR[i];
+    }
+
+    clock = adcp->config->clock ? adcp->config->clock : 10*1000*1000;
+
+    ADC->ADC_MR =
+        ADC_MR_PRESCAL( ((SystemCoreClock / 2) + (clock - 1)) / clock - 1 ) |
+        ADC_MR_STARTUP_SUT24 |
+        ADC_MR_SLEEP_NORMAL |
+        ADC_MR_LOWRES_BITS_12 |
+        ADC_MR_USEQ_NUM_ORDER |
+        ADC_MR_TRGEN_DIS;
+
+    // Enable Temp sensor on CH15
+    ADC->ADC_ACR = ADC_ACR_TSON;
+  }
+  /* Configures the peripheral.*/
+
+}
+
+/**
+ * @brief   Deactivates the ADC peripheral.
+ *
+ * @param[in] adcp      pointer to the @p ADCDriver object
+ *
+ * @notapi
+ */
+void adc_lld_stop(ADCDriver *adcp) {
+  if (adcp->state == ADC_READY) {
+    /* Resets the peripheral.*/
+    ADC->ADC_MR |= ADC_MR_SLEEP_SLEEP;
+  }
+}
+
+/**
+ * @brief   Starts an ADC conversion.
+ *
+ * @param[in] adcp      pointer to the @p ADCDriver object
+ *
+ * @notapi
+ */
+void adc_lld_start_conversion(ADCDriver *adcp) {
+  uint32_t i;
+
+  // TODO: Only software triggering for now.
+  ((ADCConversionGroup *)adcp->grpp)->circular = 0;
+  adcp->depth = 1;
+
+  /* Count the real number of activated channels in case the user got it wrong */
+  ((ADCConversionGroup *)adcp->grpp)->num_channels = 0;
+  for (i = 1; i < 0x10000 && i != 0; i <<= 1) {
+    if (adcp->grpp->channel_mask & i)
+      ((ADCConversionGroup *)adcp->grpp)->num_channels++;
+  }
+
+  /* Set the channels */
+  ADC->ADC_CHER = adcp->grpp->channel_mask;
+
+  ADC->ADC_IER = ADC_IER_GOVRE | ADC_IER_RXBUFF;
+  pdc_setup_receive(ADC, adcp->grpp->num_channels, adcp->samples);
+
+  /* Trigger */
+  ADC->ADC_CR = ADC_CR_START;
+}
+
+/**
+ * @brief   Stops an ongoing conversion.
+ *
+ * @param[in] adcp      pointer to the @p ADCDriver object
+ *
+ * @notapi
+ */
+void adc_lld_stop_conversion(ADCDriver *adcp) {
+  (void)adcp;
+  ADC->ADC_IDR = 0xFFFFFFFF;
+  ADC->ADC_CHDR = 0xFFFFFFFF;
+}
+
+#endif /* HAL_USE_ADC */
+
+/** @} */
---
+++ os/hal/platforms/SAM3XA/adc_lld.h
@@ -0,0 +1,241 @@
+/*
+    ChibiOS/RT - Copyright (C) 2013 Sam Wong
+
+    This file is part of Arduino Due patch for ChibiOS/RT.
+
+    ChibiOS/RT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS/RT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+                                      ---
+
+    A special exception to the GPL can be applied should you wish to distribute
+    a combined work that includes ChibiOS/RT, without being obliged to provide
+    the source code for any proprietary components. See the file exception.txt
+    for full details of how and when the exception can be applied.
+ */
+
+/**
+ * @file    SAM3XA/adc_lld.h
+ * @brief   SAM3XA ADC Driver subsystem low level driver header.
+ *
+ * @addtogroup ADC
+ * @{
+ */
+
+#ifndef _ADC_LLD_H_
+#define _ADC_LLD_H_
+
+#if HAL_USE_ADC || defined(__DOXYGEN__)
+
+/*===========================================================================*/
+/* Driver constants.                                                         */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver pre-compile time settings.                                         */
+/*===========================================================================*/
+
+/**
+ * @name    Configuration options
+ * @{
+ */
+/**
+ * @brief   ADC1 driver enable switch.
+ * @details If set to @p TRUE the support for ADC1 is included.
+ * @note    The default is @p FALSE.
+ */
+#if !defined(SAM3XA_ADC_USE_ADC1) || defined(__DOXYGEN__)
+#define SAM3XA_ADC_USE_ADC1                 FALSE
+#endif
+
+/**
+ * @brief   ADC interrupt priority level setting
+ */
+#if !defined(SAM3XA_PWM_DEFAULT_IRQ_PRIORITY) || defined(__DOXYGEN__)
+#define SAM3XA_ADC_IRQ_PRIORITY             12
+#endif
+/** @} */
+
+/*===========================================================================*/
+/* Derived constants and error checks.                                       */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver data structures and types.                                         */
+/*===========================================================================*/
+
+/**
+ * @brief   ADC sample data type.
+ */
+typedef uint16_t adcsample_t;
+
+/**
+ * @brief   Channels number in a conversion group.
+ */
+typedef uint8_t adc_channels_num_t;
+
+/**
+ * @brief   Possible ADC failure causes.
+ * @note    Error codes are architecture dependent and should not relied
+ *          upon.
+ */
+typedef enum {
+  ADC_ERR_DMAFAILURE = 0,                   /**< DMA operations failure.    */
+  ADC_ERR_OVERFLOW = 1                      /**< ADC overflow condition.    */
+} adcerror_t;
+
+/**
+ * @brief   Type of a structure representing an ADC driver.
+ */
+typedef struct ADCDriver ADCDriver;
+
+/**
+ * @brief   ADC notification callback type.
+ *
+ * @param[in] adcp      pointer to the @p ADCDriver object triggering the
+ *                      callback
+ * @param[in] buffer    pointer to the most recent samples data
+ * @param[in] n         number of buffer rows available starting from @p buffer
+ */
+typedef void (*adccallback_t)(ADCDriver *adcp, adcsample_t *buffer, size_t n);
+
+/**
+ * @brief   ADC error callback type.
+ *
+ * @param[in] adcp      pointer to the @p ADCDriver object triggering the
+ *                      callback
+ * @param[in] err       ADC error code
+ */
+typedef void (*adcerrorcallback_t)(ADCDriver *adcp, adcerror_t err);
+
+/**
+ * @brief   Conversion group configuration structure.
+ * @details This implementation-dependent structure describes a conversion
+ *          operation.
+ * @note    Implementations may extend this structure to contain more,
+ *          architecture dependent, fields.
+ */
+typedef struct {
+  /**
+   * @brief   Enables the circular buffer mode for the group.
+   */
+  bool_t                    circular;
+  /**
+   * @brief   Number of the analog channels belonging to the conversion group.
+   */
+  adc_channels_num_t        num_channels;
+  /**
+   * @brief   Callback function associated to the group or @p NULL.
+   */
+  adccallback_t             end_cb;
+  /**
+   * @brief   Error callback or @p NULL.
+   */
+  adcerrorcallback_t        error_cb;
+  /* End of the mandatory fields.*/
+  /**
+   * @brief   Select the ADC Channels to read.
+   * @details The number of bits at logic level one in this register must
+   *          be equal to the number in the @p num_channels field.
+   */
+  uint16_t                  channel_mask;
+} ADCConversionGroup;
+
+/**
+ * @brief   Driver configuration structure.
+ * @note    It could be empty on some architectures.
+ */
+typedef struct {
+  /**
+   * @brief ADC clock. 0 to left as default
+   * @note  Default is 10Mhz. max is 20Mhz.
+   * Please refer to the datasheet for track and transfer requirement.
+   */
+  uint32_t                  clock;
+} ADCConfig;
+
+/**
+ * @brief   Structure representing an ADC driver.
+ */
+struct ADCDriver {
+  /**
+   * @brief   Driver state.
+   */
+  adcstate_t                state;
+  /**
+   * @brief   Current configuration data.
+   */
+  const ADCConfig           *config;
+  /**
+   * @brief   Current samples buffer pointer or @p NULL.
+   */
+  adcsample_t               *samples;
+  /**
+   * @brief   Current samples buffer depth or @p 0.
+   */
+  size_t                    depth;
+  /**
+   * @brief   Current conversion group pointer or @p NULL.
+   */
+  const ADCConversionGroup  *grpp;
+#if ADC_USE_WAIT || defined(__DOXYGEN__)
+  /**
+   * @brief   Waiting thread.
+   */
+  Thread                    *thread;
+#endif
+#if ADC_USE_MUTUAL_EXCLUSION || defined(__DOXYGEN__)
+#if CH_USE_MUTEXES || defined(__DOXYGEN__)
+  /**
+   * @brief   Mutex protecting the peripheral.
+   */
+  Mutex                     mutex;
+#elif CH_USE_SEMAPHORES
+  Semaphore                 semaphore;
+#endif
+#endif /* ADC_USE_MUTUAL_EXCLUSION */
+#if defined(ADC_DRIVER_EXT_FIELDS)
+  ADC_DRIVER_EXT_FIELDS
+#endif
+  /* End of the mandatory fields.*/
+};
+
+/*===========================================================================*/
+/* Driver macros.                                                            */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* External declarations.                                                    */
+/*===========================================================================*/
+
+#if SAM3XA_ADC_USE_ADC1 && !defined(__DOXYGEN__)
+extern ADCDriver ADCD1;
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  void adc_lld_init(void);
+  void adc_lld_start(ADCDriver *adcp);
+  void adc_lld_stop(ADCDriver *adcp);
+  void adc_lld_start_conversion(ADCDriver *adcp);
+  void adc_lld_stop_conversion(ADCDriver *adcp);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HAL_USE_ADC */
+
+#endif /* _ADC_LLD_H_ */
+
+/** @} */
